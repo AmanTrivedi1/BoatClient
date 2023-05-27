3d8208b16da542285bc0c03398bd309f80840832
AmanTrivedi1 <amantrivedi598@gmail.com>
2023-05-27 22:55:11 +0530

    made some ui changes

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -4,25 +4,25 @@ import "./Banner.scss";
 import BannerImg from "../../../assets/banner-img.png";
 
 const Banner = () => {
-    return (
-        <div className="hero-banner">
-            <div className="content">
-                <div className="text-content">
-                    <h1>SALES</h1>
-                    <p>
-                        Convallis interdum purus adipiscing dis parturient
-                        posuere ac a quam a eleifend montes parturient posuere
-                        curae tempor
-                    </p>
-                    <div className="ctas">
-                        <div className="banner-cta">Read More</div>
-                        <div className="banner-cta v2">Shop Now</div>
-                    </div>
-                </div>
-                <img className="banner-img" src={BannerImg} />
-            </div>
+  return (
+    <div className="hero-banner">
+      <div className="content">
+        <div className="text-content">
+          <h4 className="tagline__start">A Voyage To</h4>
+          <h1>NIRVANA</h1>
+          <p>
+            Catch a glimpse of the range of boAt Lifestyle products in the
+            offering and choose your way of Plugging Into Nirvana!
+          </p>
+          <div className="ctas">
+            <div className="banner-cta">Read More</div>
+            <div className="banner-cta v2">Shop Now</div>
+          </div>
         </div>
-    );
+        <img className="banner-img" src={BannerImg} />
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
